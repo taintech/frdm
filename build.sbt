79f01220504e5a454b640c8e2fb8f9d881f16444
rinat.tainov <rinat.tainov@veon.com>
2017-10-01 03:11:49 +0200

    Added sbt and Main class

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "frdm"
+
+version := "0.1"
+
+scalaVersion := "2.12.3"
+        
---
+++ src/main/scala/com/taintech/frdm/Main.scala
@@ -0,0 +1,7 @@
+package com.taintech.frdm
+
+object Main extends App {
+
+  println("Hello to my FRDM practice!")
+
+}
